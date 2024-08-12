69aa794f8868bad2f81d6d00cd720325ac2778a3
jorgexleal <jorge.leal@devoteam.com>
2024-08-12 14:40:09 +0200

    Terraform added

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-recipe-app-tf-state2"
+    key            = "tf-state-setup"
+    region         = "eu-west-3"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
